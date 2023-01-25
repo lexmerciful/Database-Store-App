2b25fc67a501dd264011dbb45f2f11be8b671de9
Merciful Afolabi <lexmerciful@gmail.com>
2023-01-25 14:11:13 +0100

    Add project files.

---
+++ DB Train_/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace DB_Train_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewInfo = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.lblTeam = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtposition = new System.Windows.Forms.TextBox();
+            this.txtteam = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(74, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(692, 468);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridViewInfo
+            // 
+            this.dataGridViewInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewInfo.Location = new System.Drawing.Point(86, 62);
+            this.dataGridViewInfo.Name = "dataGridViewInfo";
+            this.dataGridViewInfo.Size = new System.Drawing.Size(429, 420);
+            this.dataGridViewInfo.TabIndex = 1;
+            this.dataGridViewInfo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewInfo_MouseClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Tomato;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(540, 330);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 30);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.TabStop = false;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.BackColor = System.Drawing.Color.Tomato;
+            this.btnRetrieve.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRetrieve.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRetrieve.Location = new System.Drawing.Point(655, 330);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(88, 30);
+            this.btnRetrieve.TabIndex = 4;
+            this.btnRetrieve.TabStop = false;
+            this.btnRetrieve.Text = "Retrieve";
+            this.btnRetrieve.UseVisualStyleBackColor = false;
+            this.btnRetrieve.Click += new System.EventHandler(this.btnRetrieve_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(539, 390);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 29);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.TabStop = false;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Tomato;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(655, 390);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(94, 29);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.TabStop = false;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Tomato;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Location = new System.Drawing.Point(598, 450);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 32);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.TabStop = false;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.White;
+            this.lblName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblName.Location = new System.Drawing.Point(536, 106);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(71, 24);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Name: ";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.BackColor = System.Drawing.Color.White;
+            this.lblPosition.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPosition.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblPosition.Location = new System.Drawing.Point(536, 173);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(86, 24);
+            this.lblPosition.TabIndex = 8;
+            this.lblPosition.Text = "Position: ";
+            // 
+            // lblTeam
+            // 
+            this.lblTeam.AutoSize = true;
+            this.lblTeam.BackColor = System.Drawing.Color.White;
+            this.lblTeam.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTeam.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeam.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblTeam.Location = new System.Drawing.Point(536, 248);
+            this.lblTeam.Name = "lblTeam";
+            this.lblTeam.Size = new System.Drawing.Size(69, 24);
+            this.lblTeam.TabIndex = 9;
+            this.lblTeam.Text = "Team: ";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(627, 101);
+            this.txtname.Multiline = true;
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(116, 29);
+            this.txtname.TabIndex = 0;
+            // 
+            // txtposition
+            // 
+            this.txtposition.Location = new System.Drawing.Point(627, 168);
+            this.txtposition.Multiline = true;
+            this.txtposition.Name = "txtposition";
+            this.txtposition.Size = new System.Drawing.Size(116, 29);
+            this.txtposition.TabIndex = 1;
+            // 
+            // txtteam
+            // 
+            this.txtteam.Location = new System.Drawing.Point(627, 243);
+            this.txtteam.Multiline = true;
+            this.txtteam.Name = "txtteam";
+            this.txtteam.Size = new System.Drawing.Size(116, 29);
+            this.txtteam.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 537);
+            this.Controls.Add(this.txtteam);
+            this.Controls.Add(this.txtposition);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.lblTeam);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRetrieve);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridViewInfo);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "DB Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridViewInfo;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.Label lblTeam;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtposition;
+        private System.Windows.Forms.TextBox txtteam;
+    }
+}
+
---
+++ DB Train_/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+
+namespace DB_Train_
+{
+    public partial class Form1 : Form
+    {
+
+        static string conString = "Server=localhost;Database=peopledb;Uid=root;pwd=;";
+        MySqlConnection con = new MySqlConnection(conString);
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        DataTable dt = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //DataGridView PROPERTIES
+
+            dataGridViewInfo.ColumnCount = 4;
+            dataGridViewInfo.Columns[0].Name = "ID";
+            dataGridViewInfo.Columns[1].Name = "Name";
+            dataGridViewInfo.Columns[2].Name = "Position";
+            dataGridViewInfo.Columns[3].Name = "Team";
+
+            dataGridViewInfo.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            //SELECTION MODE
+            dataGridViewInfo.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewInfo.MultiSelect = false;
+        }
+
+        //INSERT INTO DB
+        private void add(string name, string pos, string team)
+        {
+            //SQT STATEMENT
+            string sql = "INSERT INTO peopletb(Name,Position,Team) VALUES(@NAME,@POSITION,@TEAM)";
+            cmd = new MySqlCommand(sql, con);
+
+            //ADD PARAMETERS
+            cmd.Parameters.AddWithValue("@NAME", name);
+            cmd.Parameters.AddWithValue("@POSITION", pos);
+            cmd.Parameters.AddWithValue("@TEAM", team);
+
+            //OPEN AND EXEC INSERT
+            try
+            {
+                con.Open();
+
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    cleartxts();
+                    MessageBox.Show("Successfully Inserted!");
+                }
+
+                con.Close();
+
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        //ADD TO DGVIEW
+        private void populate(String id,String name, string pos, string team)
+        {
+            dataGridViewInfo.Rows.Add(id, name, pos, team);
+        }
+
+        //RETRIEVE FROM DB
+        private void retrieve()
+        {
+            dataGridViewInfo.Rows.Clear();
+
+            //SQL STATEMENT
+            string sql = "SELECT * FROM peopleTB";
+            cmd = new MySqlCommand(sql, con);
+
+            //OPEN CON,RETRIEVE,FILL DGVIEW
+            try
+            {
+                con.Open();
+
+                adapter = new MySqlDataAdapter(cmd);
+
+                adapter.Fill(dt);
+
+                //LOOP THROUGH DT
+                foreach(DataRow row in dt.Rows)
+                {
+                    populate(row[0].ToString(), row[1].ToString(), row[2].ToString(), row[3].ToString());
+                }
+                con.Close();
+
+                //CLEAR DT
+                dt.Rows.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        //UPDATE DB
+        private void update(int id, string name, string pos, string team)
+        {
+            //SQL STATEMENT
+            string sql = "UPDATE peopleTB SET Name='" + name + "', Position ='" + pos + "', Team='" + team + "' WHERE ID=" + id + "";
+            cmd = new MySqlCommand(sql, con);
+
+            //OPEN CON,UPDATE,RETRIEVE DGVIEW
+
+            try
+            {
+                con.Open();
+                adapter = new MySqlDataAdapter(cmd);
+
+                adapter.UpdateCommand = con.CreateCommand();
+                adapter.UpdateCommand.CommandText = sql;
+
+                if(adapter.UpdateCommand.ExecuteNonQuery()>0)
+                {
+                    cleartxts();
+                    MessageBox.Show("Successfully Updated!");
+                }
+
+                con.Close();
+
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        //DELETE FROM DB
+        private void delete(int id)
+        {
+            //SQL STATEMENT
+            string sql = "DELETE FROM peopleTB WHERE ID=" + id + "";
+            cmd = new MySqlCommand(sql, con);
+
+            //OPEN CON, DELETE, CLOSE CON
+            try
+            {
+                con.Open();
+
+                adapter = new MySqlDataAdapter(cmd);
+
+                adapter.DeleteCommand = con.CreateCommand();
+
+                adapter.DeleteCommand.CommandText = sql;
+
+                //PROMPT FOR CONFIRMATION
+                if(MessageBox.Show("Sure??","DELETE",MessageBoxButtons.OKCancel,MessageBoxIcon.Warning)==DialogResult.OK)
+                {
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        cleartxts();
+                        MessageBox.Show("Successfully Deleted!");
+                    }
+                }
+
+                con.Close();
+
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        //CLEAR TEXT
+        private void cleartxts()
+        {
+            txtname.Text = "";
+            txtposition.Text = "";
+            txtteam.Text = "";
+        }
+
+        private void dataGridViewInfo_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtname.Text = dataGridViewInfo.SelectedRows[0].Cells[1].Value.ToString();
+            txtposition.Text = dataGridViewInfo.SelectedRows[0].Cells[2].Value.ToString();
+            txtteam.Text = dataGridViewInfo.SelectedRows[0].Cells[3].Value.ToString();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            add(txtname.Text, txtposition.Text, txtteam.Text);
+        }
+
+        private void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            retrieve();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string selected = dataGridViewInfo.SelectedRows[0].Cells[0].Value.ToString();
+            int id = Convert.ToInt32(selected);
+
+            update(id, txtname.Text, txtposition.Text, txtteam.Text);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string selected = dataGridViewInfo.SelectedRows[0].Cells[0].Value.ToString();
+            int id = Convert.ToInt32(selected);
+
+            delete(id);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dataGridViewInfo.Rows.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            retrieve();
+        }
+    }
+}
